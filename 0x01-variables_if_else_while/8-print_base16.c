fa2c0126bd0d3f925c01ef757a53a1648f3a517f
Remo3311 <remo33553311@gmail.com>
2023-09-10 09:58:57 -0700

    print_base1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+	char low;
+
+	for (i = 0; i < 10; i++)
+	putchar(i);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
