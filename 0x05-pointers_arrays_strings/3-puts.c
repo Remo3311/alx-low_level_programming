7188729b9f07b7c7f362cbbb12a6a317487a806b
Remo3311 <remo33553311@gmail.com>
2023-09-19 16:02:34 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,18 @@
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
-void _puts(char *str);
-
+void _puts(char *str)
 {
-	while (*str != '\0')
+int count = 0;
+
+	while (count >= 0)
 	{
-	_putchar(*str + 0);
-	str++;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
