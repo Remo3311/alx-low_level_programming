74b818ee2fa254c7f4dc31ea9bdee1352dd08972
Remo3311 <remo33553311@gmail.com>
2023-10-02 17:59:10 -0700

    r

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[o]);
+	return (0);
+}
