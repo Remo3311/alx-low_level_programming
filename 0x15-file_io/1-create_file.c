0b05a9190be36d2f0b37ceee53df31d74c1b94ee
Remo3311 <remo33553311@gmail.com>
2023-11-14 13:35:56 -0800

    project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int thefile, int str = 0, len = 0;
+	int thefile;
+	int str = 0, len = 0;
 	char *ptr;
 
 	if (filename == NULL)
